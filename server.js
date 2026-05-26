28ec8f969955d89a21d595a85105bf2f4fa2eedd
Mohamed Samir <computermedo888@gmail.com>
2026-05-26 16:36:15 +0300

    Editing vercel file and server file 3

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ app.set('query parser', 'extended');
 const connectDB = require('./config/connectDB.js');
 const chalk = require('chalk');
 
-// 1. الاتصال بقاعدة البيانات بشكل مباشر
 connectDB();
 
-// 2. تصدير التطبيق ليتمكن Vercel من قراءته (هذا السطر يحل المشكلة!)
 module.exports = app;
 
-// 3. منع تشغيل app.listen على Vercel وتشغيله فقط على جهازك المحلي
 const PORT = process.env.PORT || 8000;
 
-// Vercel لا يستخدم وضع الـ development، لذلك سيعمل هذا فقط على جهازك
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
         console.log(chalk.green(`Server is running on port ${PORT}`));
